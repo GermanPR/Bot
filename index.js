12986e9538df0b916aebbe8601554fbe9fe18d7b
Germán Parada <Germn Parada>
2016-11-05 23:31:10 +0100

    cambios

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ intents.matches('Despedida', function (session, args, next) {
 });
 
 intents.matches('VerInventario', function (session, args, next) {
-    session.send(db.resultes);
+    session.send(collection.resultes);
 
 });
 
