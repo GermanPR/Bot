7201a445014ddcb73c4deb2447d4612c0027ffdc
Germán Parada <Germn Parada>
2016-11-07 20:38:55 +0100

    cambios MongoDB

--- index.js
+++ index.js
@@ -26,38 +26,76 @@ server.post('/api/messages', connector.listen());
 
 var mongodb = require('mongodb').MongoClient,
     docDBURL = 'mongodb://botcafeteria1:PB34wQAEOMmYRTBxdclLAY1cPN6AZi3enaV6HDOniTMDo3NY6cN7Ql0gpsKscJUridcpQRQqsIB21FWfhZQ8iA==@botcafeteria1.documents.azure.com:10250/?ssl=true';
- 
-mongodb.connect(docDBURL, function (err, db) {
- 
-    if (err) {
-        console.log('mongodb.connect error: %s', err);
-    }
-    else {
-        var Bocatas = db.collection('Bocatas');
-        
-        Bocatas.insertMany([{ name: 'Bocata de Bacon',
-                                precio: "4,50€",
-                                tipo:'comida',
-                                stock:'10' },
+var assert = require('assert');
+ 
 
-                                { name: 'Bocata de Jamón',
-                                precio: '4,5€',
-                                tipo:'comida',
-                                stock:'10' },
-                                
-                                { name: 'Bocata de Pollo',
-                                precio: "4,5€",
-                                tipo:'comida',
-                                stock:'15' }],
-                               
-                              function (err, results) {
-            if (err) {
-                console.log('Bocatas.insertMany error: %s', err);
-            }
-            else{
-                console.log(results);
-    }
+mongodb.connect(docDBURL, function (err, db) {
+     
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+
+  insertDocuments(db, function() {
+    updateDocument(db, function() {
+      deleteDocument(db, function() {
+        findDocuments(db, function() {
+          db.close();
+        });
+      });
+    });
+  });
+
 });
+var insertDocuments = function(db, callback) {
+  // Get the documents collection 
+  var collection = db.collection('documents');
+  // Insert some documents 
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the document collection");
+    callback(result);
+  });
+}
+var findDocuments = function(db, callback) {
+  // Get the documents collection 
+  var collection = db.collection('documents');
+  // Find some documents 
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    assert.equal(2, docs.length);
+    console.log("Found the following records");
+    console.dir(docs);
+    callback(docs);
+  });
+}
+var deleteDocument = function(db, callback) {
+  // Get the documents collection 
+  var collection = db.collection('documents');
+  // Insert some documents 
+  collection.deleteOne({ a : 3 }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Removed the document with the field a equal to 3");
+    callback(result);
+  });
+}
+var updateDocument = function(db, callback) {
+  // Get the documents collection 
+  var collection = db.collection('documents');
+  // Update document where a is 2, set b equal to 1 
+  collection.updateOne({ a : 2 }
+    , { $set: { b : 1 } }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Updated the document with the field a equal to 2");
+    callback(result);
+  });  
+}
+  
+
 //=========================================================
 // Bots Dialogs
 //=========================================================
@@ -96,9 +134,6 @@ intents.onDefault(function (session) {
     session.send('Lo siento, no lo he entendido.');
 });
 
-                
-            }
-  });
-
+  
 
 
