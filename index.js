20504fd05b8e1d9d066eaeb60d3df12bf5119bc4
Germán Parada <Germn Parada>
2016-11-24 15:03:02 +0100

    cambios

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function getData(callback) {
             console.log(err);
         }
         var request = new sql.Request(connection);
-        request.query('select * from postres', function (err, results) {
+        request.query('select * from postres and bebidas', function (err, results) {
             if (err) {
                 console.log(err);
             } else {
@@ -130,9 +130,11 @@ intents.matches('VerInventario', function (session, args, next) {
     getData(function (results) {
         session.send("Tenemos estos postres: ")
           for (var i = 0; i < results.length; i++) {
-              var numero = i+1;
+              session.send(results[i]);
+          }
+             /* var numero = i+1;
             session.send(numero +"-" + results[i].tipo + " : " + results[i].precio + "€");
-        }
+        }*/
         /*for (var i = 0; i < results.length; i++) {
             arrayBebidas.push(results[i].tipo);
             arrayBebidas.push(results[i].precio);
