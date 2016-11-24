fe39a40ebd6862a786342645af082b0749f030d6
Germán Parada <Germn Parada>
2016-11-24 14:44:51 +0100

    cambios

--- index.js
+++ index.js
@@ -39,12 +39,20 @@ function getData(callback) {
             console.log(err);
         }
         var request = new sql.Request(connection);
-        request.query('select * from postres', function (err, results) {
+        request.query('select * from postres', function (err, postres) {
             if (err) {
                 console.log(err);
             } else {
-                console.log("el producto es un " + results[0].tipo + " que vale " + results[0].precio + "€");
-                callback(results);
+                
+                callback(bebidas);
+            }
+        })
+        request.query('select * from bebidas', function (err, bebidas) {
+            if (err) {
+                console.log(err);
+            } else {
+                
+                callback(bebidas);
             }
         })
 
@@ -127,11 +135,16 @@ intents.matches('Pedir', function (session, args, next) {
 
 });
 intents.matches('VerInventario', function (session, args, next) {
-    getData(function (results) {
-        session.send("Tenemos estos postres: ")
-          for (var i = 0; i < results.length; i++) {
+    getPostres(function (postres,bebidas) {
+        session.send("Tenemos estas bebidas: ")
+          for (var i = 0; i < bebidas.length; i++) {
               var numero = i+1;
-            session.send(numero +"-" + results[i].tipo + " : " + results[i].precio + "€");
+            session.send(numero +"-" + bebidas[i].tipo + " : " + bebidas[i].precio + "€");
+        }
+        session.send("Tenemos estos postres: ")
+          for (var i = 0; i < postres.length; i++) {
+              var numero = i+1;
+            session.send(numero +"-" + postres[i].tipo + " : " + postres[i].precio + "€");
         }
         /*for (var i = 0; i < results.length; i++) {
             arrayBebidas.push(results[i].tipo);
