d916c3951da08a8ce5ad62c3c49686bb9a039696
Germán Parada <Germn Parada>
2016-11-30 20:24:48 +0100

    cambios

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function getName(session) {
 intents.matches('Pedir', '/pedir');
 
 bot.dialog('/pedir', [
-    function(session,args,next){
+    function (session, args, next) {
         session.send('Genial! ¿A que hora comes?');
         var msg = new builder.Message(session)
             .textFormat(builder.TextFormat.xml)
@@ -79,21 +79,24 @@ bot.dialog('/pedir', [
                         builder.CardAction.imBack(session, "14:15 - 15:15", "Seleccionar")
                     ]),
             ]);
-            builder.Prompts.choice(session,msg,"12:15 - 13:15|13:15 - 14:15|14:15 - 15:15");
+        builder.Prompts.choice(session, msg, "12:15 - 13:15|13:15 - 14:15|14:15 - 15:15");
 
     },
-    function(session,results,next){
-         session.userData.time = null;
-        switch(results.response.entity){
+    function (session, results, next) {
+        session.userData.time = null;
+        switch (results.response.entity) {
             case '12:15 - 13:15':
-              return  session.userData.time = results.response.entity;
+                session.userData.time = results.response.entity;
+                break;
             case '13:15 - 14:15':
-             return  session.userData.time = results.response.entity;
+                session.userData.time = results.response.entity;
+                break;
             case '14:15 - 15:15':
-                return  session.userData.time = results.response.entity;
-            
+                session.userData.time = results.response.entity;
+                break;
+
         }
-        
+
         // builder.Prompts.choice(session, 'Ok (y) ¿Qué te gustaría pedir?', 'Comida|Bebida|Postre');
         session.send('Ok (y) ¿Qué te gustaría pedir?');
 
@@ -169,7 +172,7 @@ bot.dialog('/pedir', [
                         ])
 
                 ]);
-            builder.Prompts.choice(session,confirmacion, "Si|No");
+            builder.Prompts.choice(session, confirmacion, "Si|No");
         }
     },
     function (session, results) {
@@ -182,7 +185,7 @@ bot.dialog('/pedir', [
                 for (var i = 0; i < session.userData.pedido.length; i++) {
                     session.send(session.userData.pedido[i]);
                 }
-                session.send("Y llegará a las **%s**",session.userData.time)
+                session.send("Y llegará a las **%s**", session.userData.time)
                 session.userData.pedido = [];
                 getConfirmation = function (pregunta) {
                     var confirmacion = new builder.Message(session)
