2fd6805331af6d089c625d52243b93aba5eac06d
Germán Parada <Germn Parada>
2016-12-06 17:07:16 +0100

    Inicial

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ module.exports = {
     // LUIS_URL: 'https://api.projectoxford.ai/luis/v1/application?id=05cc84c9-6d4a-497f-9981-cbcd438efece&subscription-key=71e4270a84a546fe814e1b0f6d4983cf',
     LUIS_URL: 'https://api.projectoxford.ai/luis/v2.0/apps/34069a65-de8e-4a04-97fa-33790df531a2?subscription-key=20fa06acaa5b45ccb587d7129da4a3a7&verbose=true',
     //Microsoft Bot Framework
-    /*AppId: '418d8392-0f66-4268-8870-a68ad9f68f44',
-    AppPassword: 'FnsKJ0JbC7EYA8vddnhFd3u'*/
-    AppId: 'f9281016-da99-4f82-b7a6-48d978f0fcc5',
-    AppPassword: '5SgnD7t00pdGX9zaZ9spHpc'
+    AppId: '418d8392-0f66-4268-8870-a68ad9f68f44',
+    AppPassword: 'FnsKJ0JbC7EYA8vddnhFd3u'
+   /* AppId: 'f9281016-da99-4f82-b7a6-48d978f0fcc5',
+    AppPassword: '5SgnD7t00pdGX9zaZ9spHpc'*/
 
 };
