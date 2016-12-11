4c13d982d15c046ab58ba16c38f356380a6b2f4a
Germán Parada <Germn Parada>
2016-12-11 22:29:59 +0100

    cambios

--- index.js
+++ index.js
@@ -301,5 +301,5 @@ function elegirHoraRecogida(session) {
         return msg;
 }
 
-/*var mysql = require('./db/sql_server.js');
-mysql.test();*/
+var mysql = require('./db/sql_server.js');
+mysql.test();
