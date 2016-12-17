41aa994139bbf67326c48203a24ea560b99770c7
Germán Parada <Germn Parada>
2016-12-17 12:51:30 +0100

    cambios

--- db/sql_server.js
+++ db/sql_server.js
@@ -95,3 +95,54 @@ exports.getData = function (session, tabla,id,field, callback) {
         connection.end();
 
 }
+
+exports.getPrice = function (session,nombre, callback) {
+
+        function getElement(params, key) {
+                for (var i = 0; i < params.length; i++) {
+                        if (params[i].indexOf(key) > -1) {
+                                return params[i].substring(params[i].indexOf('=') + 1);
+                        }
+                }
+
+                throw "Key doesn't exist!";
+        }
+
+        var params = mySQLconnString.split(';'),
+                dbhost = getElement(params, 'Data Source'),
+                dbport = dbhost.substring(dbhost.indexOf(':') + 1),
+                dbhost = dbhost.substring(0, dbhost.indexOf(':')); //host without port    
+
+        var connection = mysql.createConnection({
+                host: dbhost,
+                port: dbport,
+                user: getElement(params, 'User Id'),
+                password: getElement(params, 'Password'),
+                database: getElement(params, 'Database')
+
+        });
+
+        connection.connect(function (error) {
+                if (error) {
+                        console.error(error);
+                }
+        });
+        connection.query('SELECT * from producto where Nombre='+nombre, function (err, results) {
+                var resultados = [];
+                if (!err) {
+                        for (var i = 0; i < results.length; i++) {
+                                resultados.push(results[i].Precio);
+                                
+                        }
+                        callback(null,resultados);
+                }
+                else {
+                        console.log('error:', err);
+                }
+        });
+
+
+        connection.end();
+
+}
+
